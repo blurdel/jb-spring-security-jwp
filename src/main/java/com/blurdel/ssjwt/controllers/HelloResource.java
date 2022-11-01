e01f8447f3415903409c96785905c7355d3de1bf
blurdel <5648737+blurdel@users.noreply.github.com>
2022-11-01 09:15:01 -0400

    initial commit

---
+++ src/main/java/com/blurdel/ssjwt/controllers/HelloResource.java
@@ -0,0 +1,15 @@
+package com.blurdel.ssjwt.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloResource {
+
+	@GetMapping({ "/hello" })
+	public String getHello() {
+		return "Hello World with JWT";
+	}
+	
+}
